7ccf1a4ed1b59769c5671224ee5a73add61729f0
soos-nagymark <soos-nagy.mark@ganziskola.hu>
2026-03-23 12:47:59 +0100

    auth 1

--- projektbeadando/ReceptMegosztoSolution/ReceptekLibrary/obj/Debug/net8.0/ReceptekLibrary.AssemblyInfo.cs
+++ projektbeadando/ReceptMegosztoSolution/ReceptekLibrary/obj/Debug/net8.0/ReceptekLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReceptekLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfcf95ac1e7fc6660d6008a671fe46150c416e5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f80bf8da95c7a1ac7fbf8b29bb0d1c9099742171")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReceptekLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReceptekLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
