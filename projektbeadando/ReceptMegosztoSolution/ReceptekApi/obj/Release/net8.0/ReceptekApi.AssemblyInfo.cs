6c3b5fdf84fdc8e52421200a7a1b88853dd9fc63
Takos05 <tabiakos05@gmail.com>
2026-03-29 16:55:04 +0200

    felhasználónevek

--- projektbeadando/ReceptMegosztoSolution/ReceptekApi/obj/Release/net8.0/ReceptekApi.AssemblyInfo.cs
+++ projektbeadando/ReceptMegosztoSolution/ReceptekApi/obj/Release/net8.0/ReceptekApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReceptekApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e425fc1e5d3017be284125cbac8b8d66c0469431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9666f310ef98c8869f54a9d599e7505a9273a1a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReceptekApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReceptekApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
