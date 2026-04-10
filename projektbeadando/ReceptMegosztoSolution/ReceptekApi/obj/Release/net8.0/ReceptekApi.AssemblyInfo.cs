cad4dd248d4a7f5c23c42c24254f1a2e957d2fcf
soos-nagymark <soos-nagy.mark@ganziskola.hu>
2026-04-10 18:32:21 +0200

    nyilatkozat

--- projektbeadando/ReceptMegosztoSolution/ReceptekApi/obj/Release/net8.0/ReceptekApi.AssemblyInfo.cs
+++ projektbeadando/ReceptMegosztoSolution/ReceptekApi/obj/Release/net8.0/ReceptekApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReceptekApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afae0c63a1a9f2b81a1ea0f4114eb5d6203914dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0985e456ed8cf8ac1183c4035ee2c9cf9f76f562")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReceptekApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReceptekApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
